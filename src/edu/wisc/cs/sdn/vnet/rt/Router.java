80bbf467eb2e37a208a9b64b95a3dc214351e074
Gage Krumbach <gkrumbach@gmail.com>
2021-10-18 18:59:28 -0500

    test code

--- src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -136,8 +136,6 @@ public class Router extends Device
 							// set packet source as port/interface mac-addr
 							etherPacket.setSourceMACAddress(matchingEntry.getInterface().getMacAddress().toBytes());
 
-							System.out.println(etherPacket.toString());
-
 							// send packet out
 							sendPacket(etherPacket, matchingEntry.getInterface());
 						}
